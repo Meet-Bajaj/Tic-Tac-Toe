4101f9f0d00fe6818d0b73d9e1a2fe113445a599
Meet-Bajaj <Meetbajaj005@gmail.com>
2024-04-14 19:55:14 +0530

    Move CSS and JS to assets

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// let popup = document.getElementById("popup");
+// let blocks = document.getElementById.value("block");
+// function displayXO(){
+//     blocks = " X ";
+// }
+// function playagain() {
+//     popup.classList.add("openpopup")
+// }
+// function reset() {
+//     popup.classList.remove("openpopup")
+// }
